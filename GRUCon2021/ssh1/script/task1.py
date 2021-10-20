b6e51a8d0d44a2f5aee98452654f0c7c0e4564b5
BlackVS <vvs@coders.in.ua>
2021-10-20 07:52:48 +0300

    GRUCon2021 ssh1 script

---
+++ GRUCon2021/ssh1/script/task1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os, sys
+from collections import Counter
+from pyhamtools import LookupLib, Callinfo
+
+res_reporter = Counter()
+
+res_maxdst = 0
+res_reporter_most = None
+res_most_distant = None
+
+with open("../input/wsprspots.csv","rt") as f:
+    lines=f.readlines()
+    for l in lines[1:]:
+        l=l.strip()
+        reporter, sdistance, callsign, reporter_grid, grid = l.split(',')
+        distance = int(sdistance)
+        res_reporter[reporter] +=1
+    res_reporter_most = res_reporter.most_common(1)[0][0]
+
+    my_lookuplib = LookupLib(lookuptype="countryfile")
+    cic = Callinfo(my_lookuplib)
+    try:
+        print("Reporter with the most reports located in: {}, {}".format(res_reporter_most, cic.get_country_name(res_reporter_most)))
+    except:
+        pass
